94e6ede0d6c5fef48bb13073b2fe2add8ca86cda
phucandell <phucanvn2012@gmail.com>
2019-07-31 09:49:25 +0700

    first commit

---
+++ imports/ui/App.js
@@ -0,0 +1,4 @@
+import {Meteor} from 'meteor';
+import React,{Component} from 'react';
+import {withTracker} from 'meteor/react-meteor-data';
+import {DragDropContext} from 'react-beautiful-dnd';//Beautiful Drag and Drop npm package
---
+++ server/main.js
@@ -0,0 +1,8 @@
+import { Meteor } from 'meteor/meteor';
+
+import '../imports/api';
+import './publications';
+
+Meteor.startup(() => {
+    // code to run on server at startup
+});
